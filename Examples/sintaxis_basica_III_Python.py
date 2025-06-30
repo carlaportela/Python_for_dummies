01c02928afde9b202fa72db54f5cc992026cae8a
Carla Portela Ubeira 🔰 <120684708+carlaportela@users.noreply.github.com>
2025-06-30 14:57:05 +0200

    Add files via upload

---
+++ Examples/sintaxis_basica_III_Python.py
@@ -0,0 +1,10 @@
+#Funciones predeterminadas
+print("Este programa muestra cómo se crean funciones en Python.")
+
+#Fuciones propias
+def mensaje():
+    print("Hola, esto es una función propia.")
+
+#Llamando a la función
+mensaje()
+
