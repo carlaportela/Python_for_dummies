c51ae24bb8fbc8594459e015756a423b250ff405
Carla Portela Ubeira 🔰 <120684708+carlaportela@users.noreply.github.com>
2025-07-17 09:39:44 +0200

    Add files via upload

---
+++ Examples/Mi_primer_paquete/Subpaquete_modulos/operaciones_cadenas.py
@@ -0,0 +1,11 @@
+def unirCadenas(cadena1, cadena2):
+    print("Unión de cadenas: " + cadena1 + " " + cadena2)
+
+def longitudCadena(cadena):
+    print("Longitud de la cadena: " + str(len(cadena)))
+
+def convertirMayusculas(cadena):
+    print("Cadena en mayúsculas: " + cadena.upper())
+    
+def convertirMinusculas(cadena):
+    print("Cadena en minúsculas: " + cadena.lower())
